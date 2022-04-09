6533d82003c90f360c14ba96f3c16eed525b3bc3
smocgeorgemarian <george.smoc13@gmail.com>
2022-04-09 19:55:48 +0300

    Update Smoc

--- TwProject/navBarScript.js
+++ TwProject/navBarScript.js
@@ -1,83 +1,13 @@
-#NavBar {
-    width: 100%;
+
+function setSpinnable(object) {
+    // object.classList.remove("fa fa-home");
 }
 
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: grey;
-    opacity: 80%;
+//Form Manager
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
 }
 
-li.Normal-link {
-    float: left;
-}
-
-li a, .dropbtn {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
-
-li a:hover, .Dropdown:hover .dropbtn {
-    background-color: red;
-}
-
-li.Dropdown {
-    float: right;
-}
-
-.Dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: white;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-    z-index: 1;
-}
-
-.Dropdown-content a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    text-align: left;
-}
-
-.Dropdown-content a:hover {
-    background-color: #f1f1f1;
-}
-
-.Dropdown:hover .Dropdown-content {
-    display: block;
-}
-
-.search-container {
-    float: right;
-}
-
-#NavBar input[type=text] {
-    padding: 6px;
-    margin-top: 8px;
-    font-size: 17px;
-    border: none;
-}
-
-#NavBar .search-container button {
-    float: right;
-    padding: 6px 10px;
-    margin-top: 8px;
-    margin-right: 16px;
-    background: #ddd;
-    font-size: 17px;
-    border: none;
-    cursor: pointer;
-}
-
-#NavBar .search-container button:hover {
-    background: #ccc;
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
 }
