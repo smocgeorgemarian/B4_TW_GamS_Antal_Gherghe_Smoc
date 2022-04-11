9fcf90e4e40e4f71e027a23c3b45df590e46474b
smocgeorgemarian <george.smoc13@gmail.com>
2022-04-11 12:14:33 +0300

    Update setting dinamically generated

--- TwProject/scripts/settings.js
+++ TwProject/scripts/settings.js
@@ -1,6 +1,40 @@
 
-function setSettingsPrintable() {
-    document.getElementById('s').onclick = function() {
-        alert("button was clicked");
-    };
+function setAddSettingsPrintable() {
+    let parent = document.getElementById("settings");
+    let exists = false;
+    let foundChild;
+    [].forEach.call(parent.childNodes, function(child) {
+        console.log(child.tagName);
+        if (child.className === "settings") {
+            exists = true;
+            foundChild = child;
+        }
+    });
+    if(!exists){
+        let settingsDiv = document.createElement("div");
+        settingsDiv.classList.add("settings");
+
+        let ul = document.createElement("ul");
+        let li = document.createElement("li");
+
+        let lable = document.createElement("lable");
+        let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+
+        let text = document.createTextNode("setting_1");
+        li.appendChild(checkbox);
+        li.appendChild(text);
+        ul.appendChild(li);
+        settingsDiv.appendChild(ul);
+        parent.appendChild(settingsDiv);
+
+    }else{
+        parent.removeChild(foundChild);
+    }
 }
+
+// <ul>
+//     <li><label className="ReportRadioButton"><input type="checkbox">Nudity</label></li>
+//     <li><label className="ReportRadioButton"> <input type="checkbox">Bad Words</label></li>
+//     <li><label className="ReportRadioButton"><input type="checkbox">Religious propaganda</label></li>
+// </ul>
