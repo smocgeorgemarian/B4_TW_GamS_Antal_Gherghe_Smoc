a01fa21d577ca4073ddc4b575015b87f4de0f37f
Tudor-Alexandru-B4 <tudor.gherghe@yahoo.com>
2022-06-15 19:46:31 +0300

    am terminat pachetul api_users

--- PLSQL/WEB_API_USERS.sql
+++ PLSQL/WEB_API_USERS.sql
@@ -1,12 +1,46 @@
 CREATE OR REPLACE PACKAGE api_users AS
-    FUNCTION owner_register(owner_name VARCHAR2, owner_password VARCHAR2) RETURN VARCHAR2;
+    FUNCTION owner_login(owner_name VARCHAR2, owner_password VARCHAR2) RETURN VARCHAR2;
+    FUNCTION owner_logout(owner_name VARCHAR2) RETURN VARCHAR2;
+    FUNCTION owner_register(owner_name VARCHAR2, owner_password VARCHAR2, owner_site_link VARCHAR2) RETURN VARCHAR2;
+    FUNCTION owner_delete(owner_name VARCHAR2, owner_password VARCHAR2) RETURN VARCHAR2;
 END api_users;
 
 /
 
 CREATE OR REPLACE PACKAGE BODY api_users AS
     
-    FUNCTION owner_register(owner_name VARCHAR2, owner_password VARCHAR2)
+    FUNCTION owner_login(owner_name VARCHAR2, owner_password VARCHAR2)
+    RETURN VARCHAR2 AS
+        good_login INTEGER;
+        returner VARCHAR2(200);
+    BEGIN       
+        SELECT COUNT(*) INTO good_login FROM OWNERS WHERE oname = owner_name AND opassword = owner_password AND is_logged = 0;
+        IF good_login = 0 THEN
+            returner := '0';
+            IF test_existence.test_owner(owner_name) = 0 THEN
+                returner := '404';
+            END IF;
+        ELSE
+            returner := '1';
+            UPDATE OWNERS SET is_logged = 1 WHERE oname = owner_name;
+        END IF;   
+        RETURN returner;       
+    END owner_login;
+    
+    FUNCTION owner_logout(owner_name VARCHAR2)
+    RETURN VARCHAR2 AS
+        returner VARCHAR2(200);
+    BEGIN
+        IF test_existence.test_owner(owner_name) = 0 THEN
+            returner := '404';
+        ELSE
+            returner := '1';
+            UPDATE OWNERS SET is_logged = 0 WHERE oname = owner_name;
+        END IF;
+        RETURN returner;
+    END owner_logout;
+    
+    FUNCTION owner_register(owner_name VARCHAR2, owner_password VARCHAR2, owner_site_link VARCHAR2)
     RETURN VARCHAR2 AS
         returner VARCHAR2(200);
         hash_owner VARCHAR2(200);
@@ -14,12 +48,29 @@ CREATE OR REPLACE PACKAGE BODY api_users AS
         IF test_existence.test_owner(owner_name) = 1 THEN
             returner := '0';
         ELSE
-            returner := '1' || ' ';
-            INSERT INTO OWNERS(oname, opassword) VALUES(owner_name, owner_password);
+            INSERT INTO OWNERS(oname, opassword, site_link, is_logged) VALUES(owner_name, owner_password, owner_site_link, 0);
             SELECT hash_code INTO hash_owner FROM OWNERS WHERE oname = owner_name;
-            returner := returner || ' ' || hash_owner;
+            returner :=  hash_owner;
         END IF;
         RETURN returner;
     END owner_register;
+    
+    FUNCTION owner_delete(owner_name VARCHAR2, owner_password VARCHAR2)
+    RETURN VARCHAR2 AS
+        returner VARCHAR2(200);
+        hashcode VARCHAR2(200);
+        status INTEGER;
+    BEGIN
+        
+        SELECT COUNT(*) INTO status FROM OWNERS WHERE oname = owner_name AND opassword = Owner_password;
+        IF status = 0 THEN
+            returner := '0';
+        ELSE
+            returner := '1';
+            SELECT hash_code INTO hashcode FROM OWNERS WHERE oname = owner_name AND opassword = Owner_password;
+            table_deletion.delete_owner_info(hashcode);
+        END IF;
+        RETURN returner;
+    END owner_delete;
     
 END api_users;
--- PLSQL/WEB_MAIN.sql
+++ PLSQL/WEB_MAIN.sql
@@ -2,6 +2,8 @@ DROP TABLE OWNERS;
 CREATE TABLE OWNERS(
     oname VARCHAR2(200) PRIMARY KEY,
     opassword VARCHAR2(200),
+    site_link VARCHAR2(500),
+    is_logged NUMBER(1, 0),
     hash_code VARCHAR(200) UNIQUE
 );
 
@@ -23,6 +25,17 @@ DELETE FROM OWNERS;
 INSERT INTO OWNERS(oname, opassword) VALUES ('GiGi', 'OiOi');
 SELECT * FROM OWNERS;
 
+--DEMO API_USERS
+set serveroutput on;
+BEGIN
+    --dbms_output.put_line(api_users.owner_register('Tudor','pass','www.pornhub.com'));
+    --dbms_output.put_line(api_users.owner_login('Tudor','pass'));
+    --dbms_output.put_line(api_users.owner_logout('Tudor'));
+    --dbms_output.put_line(api_users.owner_delete('Tudor','pass'));
+END;
+
+SELECT * FROM OWNERS;
+
 set serveroutput on;
 BEGIN
     --table_insertor.insert_event('TWIwZSFkty', 'testnou', 'sum', 69);
@@ -32,9 +45,8 @@ BEGIN
     --table_insertor.insert_update('testnou', 'TWIwZSFkty', 'user1', 69);
     --dbms_output.put_line(rewards.event('testnou', 'TWIwZSFkty', 'user1'));
     --dbms_output.put_line(rewards.get_rewards('TWIwZSFkty', 'user1'));
-    dbms_output.put_line(api_users.owner_register('test','pass'));
+    --dbms_output.put_line(api_users.owner_register('test','pass'));
 END;
-
 
 
 
