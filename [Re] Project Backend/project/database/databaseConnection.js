4ad059f3a1d6a913b591b809ac57af01037a56d9
Tudor-Alexandru-B4 <tudor.gherghe@yahoo.com>
2022-06-17 03:22:13 +0300

    Backand and Postman for API USERS

--- [Re] Project Backend/project/database/databaseConnection.js
+++ [Re] Project Backend/project/database/databaseConnection.js
@@ -1,11 +1,14 @@
 const oracledb = require('oracledb');
 
-async function connectToDatabase(username, password) {
+async function users_register(username, password, site) {
     let connection
     try {
-        connection = await oracledb.getConnection({ user: "PA", password: "PA", connectionString: "localhost/xe" });
+        connection = await oracledb.getConnection({ user: "tudor", password: "tudor", connectionString: "localhost/xe" });
         console.log("Successfully connected to Oracle Database");
-        connection.execute(`begin api_users.owner_register('care1', 'care1'); commit; end;`)
+        let result = connection.execute(`SELECT api_users.owner_register('${username}', '${password}', '${site}') FROM DUAL`)
+        //await result;
+        let response = (await result).rows[0][0];
+        console.log(response)
     } catch (err) {
         console.error(err);
     } finally {
@@ -19,7 +22,75 @@ async function connectToDatabase(username, password) {
     }
 }
 
+async function users_login(username, password) {
+    let connection
+    try {
+        connection = await oracledb.getConnection({ user: "tudor", password: "tudor", connectionString: "localhost/xe" });
+        console.log("Successfully connected to Oracle Database");
+        let result = connection.execute(`SELECT api_users.owner_login('${username}', '${password}') FROM DUAL`)
+        //await result;
+        let response = (await result).rows[0][0];
+        console.log(response)
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+}
+
+async function users_logout(username) {
+    let connection
+    try {
+        connection = await oracledb.getConnection({ user: "tudor", password: "tudor", connectionString: "localhost/xe" });
+        console.log("Successfully connected to Oracle Database");
+        let result = connection.execute(`SELECT api_users.owner_logout('${username}') FROM DUAL`)
+        //await result;
+        let response = (await result).rows[0][0];
+        console.log(response)
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+}
+
+async function users_delete(username, password) {
+    let connection
+    try {
+        connection = await oracledb.getConnection({ user: "tudor", password: "tudor", connectionString: "localhost/xe" });
+        console.log("Successfully connected to Oracle Database");
+        let result = connection.execute(`SELECT api_users.owner_delete('${username}', '${password}') FROM DUAL`)
+        //await result;
+        let response = (await result).rows[0][0];
+        console.log(response)
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+}
 
 module.exports = {
-    connectToDatabase
+    users_register,
+    users_login,
+    users_logout,
+    users_delete
 }
